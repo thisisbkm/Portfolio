e40dd05b0275178655fcc0ce57e935a711e287bc
thisisbkm <bharatkumarmareedu@gmail.com>
2023-01-20 16:33:33 +0530

    skills

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,3 +19,22 @@ function linkAction(){
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n=>n.addEventListener('click', linkAction));
+
+
+const skillsContent = document.getElementsByClassName('skills_content'),
+    skillsHeader = document.querySelectorAll('.skills_header');
+
+
+function toggleSkills(){
+    let itemClass = this.parentNode.getElementsByClassName;
+    for(let i = 0;i<skillsContent.length;i++){
+        skillsContent[i].className = 'skills_content skills_close';
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open';
+    }
+}
+
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click', toggleSkills);
+})
