7c7372d01655e28cf187d71d81aaa64ed7a3602c
thisisbkm <bharatkumarmareedu@gmail.com>
2023-01-20 20:21:58 +0530

    s

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,7 @@ if(navClose){
         navMenu.classList.remove('show-menu');
     })
 }
-
+ 
 const navLink = document.querySelectorAll('.nav_link')
 function linkAction(){
     const navMenu = document.getElementById('nav-menu');
@@ -24,17 +24,55 @@ navLink.forEach(n=>n.addEventListener('click', linkAction));
 const skillsContent = document.getElementsByClassName('skills_content'),
     skillsHeader = document.querySelectorAll('.skills_header');
 
+var skillsOpen = 'skills_content skills_open', 
+    skillsClose = 'skills_content skills_close';
 
 function toggleSkills(){
-    let itemClass = this.parentNode.getElementsByClassName;
-    for(let i = 0;i<skillsContent.length;i++){
-        skillsContent[i].className = 'skills_content skills_close';
-    }
-    if(itemClass === 'skills_content skills_close'){
-        this.parentNode.className = 'skills_content skills_open';
+    let itemClass = this.parentNode;
+    if(itemClass.className === skillsOpen){
+        itemClass.className = skillsClose;
+    }else{
+        console.log(skillsContent);
+        Array.from(skillsContent).forEach((val)=>{
+            val.className = skillsClose;
+        })
+        itemClass.className = skillsOpen;
     }
 }
 
 skillsHeader.forEach((el)=>{
     el.addEventListener('click', toggleSkills);
+})
+
+function scrollTop(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY>=560) scrollUp.classList.add('show-scroll');else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop);
+
+
+// theme
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = ()=> document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = ()=> document.body.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add':'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add':'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', ()=>{
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
 })
